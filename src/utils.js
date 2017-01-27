153ff55ade2723732e342359a0755906ee1968d7
Youngchan Je <ochanje210@gmail.com>
2017-01-28 00:35:04 +0900

    Delete utils.js

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export const URL = 'http://52.78.226.183:5002/graphql';
-export const config = {
-  apiKey: 'AIzaSyDDhknkw2Q-yXX6UzRFpMXpp4pYCSxfvh4',
-  authDomain: 'vinyl-73696.firebaseapp.com',
-  databaseURL: 'https://vinyl-73696.firebaseio.com',
-  storageBucket: 'vinyl-73696.appspot.com',
-  messagingSenderId: '960372413712'
-};
