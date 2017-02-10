17720b337715fb546186b3cf383d8d39c5ee7c15
Youngchan Je <ochanje210@gmail.com>
2017-02-10 22:24:55 +0900

    added android native vinylmap module skeleton

---
+++ android/app/src/main/java/com/pingstersapp/VinylMap/VinylMapPackage.java
@@ -0,0 +1,38 @@
+package com.pingstersapp.VinylMap;
+
+/**
+ * Created by jeyoungchan on 2/10/17.
+ */
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.pingstersapp.VinylMap.VinylMapModule;
+
+public class VinylMapPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new VinylMapModule(reactContext));
+
+        return modules;
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
