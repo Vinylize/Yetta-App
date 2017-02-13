b4600e6a44e20e1e0ad5409adde2fd9974bd06f8
Youngchan Je <ochanje210@gmail.com>
2017-02-13 17:44:46 +0900

    added entire view shrinking animation

--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,8 @@ import {
   Platform,
   NativeModules,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  Animated
 } from 'react-native';
 import * as firebase from 'firebase';
 import {
@@ -33,35 +34,35 @@ export default class Home extends Component {
     super();
     this.state = {
       text: '',
-      toggle: false
+      toggle: false,
+      longitude: undefined,
+      latitude: undefined,
+      menuClicked: false,
+      shrinkValue: new Animated.Value(1)
     };
   }
 
   componentDidMount() {
-    if (this.state.first) {
-      firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-          // firebase.auth().getToken().then(console.log);
-          this.props.navigator.push(portOrShipNavigatorRoute());
-        } else {
-          // TBD
-        }
-      });
-    }
+    // if (this.state.first) {
+    //   firebase.auth().onAuthStateChanged((user) => {
+    //     if (user) {
+    //       // firebase.auth().getToken().then(console.log);
+    //       this.props.navigator.push(portOrShipNavigatorRoute());
+    //     } else {
+    //       // TBD
+    //     }
+    //   });
+    // }
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const { longitude, latitude } = position.coords;
         this.setState({
-          center: {
-            longitude,
-            latitude
-          },
-          gotUserLocation: true
+          longitude: longitude,
+          latitude: latitude
         });
       },
       (error) => {
         console.log(JSON.stringify(error));
-        this.setState({gotUserLocation: true});
       },
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     );
@@ -113,7 +114,7 @@ export default class Home extends Component {
         left: (WIDTH - WIDTH * 0.5) / 2,
         top: 40,
         width: WIDTH * 0.5,
-        height: 35,
+        height: 30,
         backgroundColor: '#75797a',
         borderRadius: 20,
         shadowOffset: {height: 1, width: 1},
@@ -135,7 +136,7 @@ export default class Home extends Component {
               position: 'absolute',
               top: 2,
               width: WIDTH * 0.25,
-              height: 31,
+              height: 31 - 5,
               backgroundColor: 'white',
               borderRadius: 20,
           }, (toggle) ? {right: 4} : {left: 4}]}/>
@@ -160,12 +161,153 @@ export default class Home extends Component {
     )
   }
 
+  renderLocationBtn() {
+    return (
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          right: 26,
+          bottom: 80,
+          height: 25,
+          width: 25,
+          borderRadius: 20,
+          backgroundColor: '#2E3031',
+          shadowOffset: {height: 1, width: 1},
+          shadowOpacity: 0.2
+        }}
+        activeOpacity={0.8}
+        onPress={() => {
+          const { latitude, longitude } = this.state;
+          vmm.animateToLocation(String(latitude), String(longitude));
+        }}
+      >
+
+      </TouchableOpacity>
+    )
+  }
+
+  renderMenu() {
+    const { menuClicked } = this.state;
+    return (
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          left: 20,
+          top: 46,
+          backgroundColor: 'transparent',
+          width: 24,
+          height: 20
+        }}
+        onPress={() => {
+          LayoutAnimation.easeInEaseOut();
+          this.setState({menuClicked: !menuClicked});
+          if (this.state.menuClicked) {
+            this.animateBack();
+          } else {
+            this.animateShrink();
+          }
+        }}
+        activeOpacity={1}
+      >
+        <View style={(menuClicked) ? {
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: 15,
+          height: 3,
+          backgroundColor: '#2E3031',
+          transform: [{rotate: '45deg'}]
+          } : {
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: 24,
+          height: 3,
+          backgroundColor: '#2E3031',
+        }}/>
+        <View style={(menuClicked) ? {
+          position: 'absolute',
+          left: 9,
+          top: 0,
+          width: 15,
+          height: 3,
+          backgroundColor: '#2E3031',
+          transform: [{rotate: '-45deg'}]
+          } : {
+          position: 'absolute',
+          left: 0,
+          top: 7,
+          width: 24,
+          height: 3,
+          backgroundColor: '#2E3031',
+        }}/>
+        <View style={(menuClicked) ? {
+          position: 'absolute',
+          left: 4.5,
+          top: 12,
+          width: 15,
+          height: 3,
+          backgroundColor: '#2E3031',
+          transform: [{rotate: '90deg'}]
+          } : {
+          position: 'absolute',
+          left: 0,
+          top: 14,
+          width: 24,
+          height: 3,
+          backgroundColor: '#2E3031',
+        }}/>
+      </TouchableOpacity>
+    )
+  }
+
+  animateShrink() {
+    console.log('s');
+    this.state.shrinkValue.setValue(1);
+    Animated.timing(
+      this.state.shrinkValue,
+      {
+        toValue: 0.7,
+        duration: 100
+      }
+    ).start();
+  }
+
+  animateBack() {
+    console.log('asdf');
+    this.state.shrinkValue.setValue(0.7);
+    Animated.timing(
+      this.state.shrinkValue,
+      {
+        toValue: 1,
+        duration: 100
+      }
+    ).start();
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        {this.renderMap()}
-        {this.renderSwitch()}
-        {this.renderSearchBar()}
+      <View style={{flex: 1, backgroundColor: '#2E3031'}}>
+        <Animated.View style={(this.state.menuClicked) ? {
+          flex: 1, left: 20, transform: [{scale: this.state.shrinkValue}]
+          } : {flex: 1, transform: [{scale: this.state.shrinkValue}]}}>
+          {this.renderMap()}
+          {this.renderMenu()}
+          {this.renderSwitch()}
+          {this.renderSearchBar()}
+          {this.renderLocationBtn()}
+          <View style={{
+            position: 'absolute',
+            right: 20,
+            bottom: 20,
+            height: 40,
+            width: 40,
+            borderRadius: 50,
+            backgroundColor: 'white',
+            shadowOffset: {height: 1, width: 2},
+            shadowOpacity: 0.23
+          }}/>
+        </Animated.View>
       </View>
     );
   }
