82ac773bcc7c1e5d0c240fbc3f2231ea8df278f8
Youngchan Je <ochanje210@gmail.com>
2017-03-14 17:50:10 +0900

    iOS: FCM basic setup (#41)
    * iOS: FCM basic setup
    
    * update
    
    * update gitignore

--- ios/pingstersApp/AppDelegate.m
+++ ios/pingstersApp/AppDelegate.m
@@ -13,8 +13,39 @@
 #import "RCTRootView.h"
 
 @import GoogleMaps;
+@import Firebase;
+@import UserNotifications;
 
 @implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(nullable NSDictionary *)launchOptions
+{
+  if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
+    UIUserNotificationType allNotificationTypes =
+    (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+    UIUserNotificationSettings *settings =
+    [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+  } else {
+    // iOS 10 or later
+    #if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+    // For iOS 10 display notification (sent via APNS)
+    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+    UNAuthorizationOptions authOptions =
+      UNAuthorizationOptionAlert
+      | UNAuthorizationOptionSound
+      | UNAuthorizationOptionBadge;
+    [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    }];
+    
+    // For iOS 10 data message (sent via FCM)
+    [FIRMessaging messaging].remoteMessageDelegate = self;
+    #endif
+  }
+  
+  [[UIApplication sharedApplication] registerForRemoteNotifications];
+  return YES;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -35,6 +66,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
   return YES;
 }
 
