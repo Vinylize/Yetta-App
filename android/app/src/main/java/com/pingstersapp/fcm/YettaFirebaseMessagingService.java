8dc5123f910be96a33cb77c08ccd04f3baa18fcf
Youngchan Je <ochanje210@gmail.com>
2017-03-15 11:03:00 +0900

    delete system.out

--- android/app/src/main/java/com/pingstersapp/fcm/YettaFirebaseMessagingService.java
+++ android/app/src/main/java/com/pingstersapp/fcm/YettaFirebaseMessagingService.java
@@ -41,7 +41,7 @@ public class YettaFirebaseMessagingService extends FirebaseMessagingService {
         // and data payloads are treated as notification messages. The Firebase console always sends notification
         // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
         // [END_EXCLUDE]
-        System.out.println("메세지 테스트11");
+
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
