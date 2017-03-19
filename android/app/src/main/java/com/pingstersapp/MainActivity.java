779d6f611bc09ab8e4ccdb2709ec46543329c26a
Youngchan Je <ochanje210@gmail.com>
2017-03-19 14:55:05 +0900

    Android: handled FCM when app is in background and killed using intent

--- android/app/src/main/java/com/pingstersapp/MainActivity.java
+++ android/app/src/main/java/com/pingstersapp/MainActivity.java
@@ -1,8 +1,37 @@
 package com.pingstersapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
 import com.facebook.react.ReactActivity;
 
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        if (getIntent().getExtras() != null) {
+            System.out.println(getIntent().getExtras());
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+        // [END handle_data_extras]
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -11,5 +40,17 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "pingstersApp";
+    }
+
+    @Override
+    public void onNewIntent (Intent intent) {
+        super.onNewIntent(intent);
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+            setIntent(intent);
     }
 }
