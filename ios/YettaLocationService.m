200c3e18f50bff89d2c4954f0a2176e246f01c87
Youngchan Je <ochanje210@gmail.com>
2017-03-21 11:49:14 +0900

    added location update on app in foreground/background/terminated

---
+++ ios/YettaLocationService.m
@@ -0,0 +1,126 @@
+//
+//  YettaLocationService.m
+//  pingstersApp
+//
+//  Created by Youngchan Je on 3/21/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+#import "YettaLocationService.h"
+
+
+@implementation YettaLocationService
+
+@synthesize locationManager, currentLocation;
+
+- (void)startLocationService {
+  locationManager = [[CLLocationManager alloc] init];
+  locationManager.delegate = self;
+  
+  CLAuthorizationStatus authorizationStatus= [CLLocationManager authorizationStatus];
+  // todo: change the following condition to handle more detailed situations
+  if (authorizationStatus != kCLAuthorizationStatusAuthorizedAlways)
+    [locationManager requestAlwaysAuthorization];
+  
+  if ([CLLocationManager locationServicesEnabled]) {
+    // [locationManager startUpdatingLocation];
+    NSLog(@"start location update");
+  } else {
+    NSLog(@"Location services is not enabled");
+  }
+  
+  [self startSignificantChangeUpdates];
+}
+
+- (void)startSignificantChangeUpdates
+{
+  // Create the location manager if this object does not already have one.
+  if (nil == locationManager)
+    locationManager = [[CLLocationManager alloc] init];
+  
+  locationManager.delegate = self;
+  [locationManager startMonitoringSignificantLocationChanges];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+  self.currentLocation = newLocation;
+  
+  NSLog(@"Latidude %f Longitude: %f", newLocation.coordinate.latitude, newLocation.coordinate.longitude);
+  
+  [self saveNotification:@"test notification" :@"unique id" :NO :newLocation.coordinate];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+  [locationManager stopUpdatingLocation];
+  NSLog(@"Update failed with error: %@", error);
+}
+
+#ifdef DEBUG
+-(void)saveNotification:(NSString *)description :(NSString *)notificationID :(BOOL)locationCheck :(CLLocationCoordinate2D)coordinates{
+  
+  // Create the notification info dictionary
+  // and set the notification ID string.
+  NSMutableDictionary *userInfo = [[NSMutableDictionary alloc] init];
+  [userInfo setObject:notificationID forKey:@"notification_id"];
+  
+  [userInfo setObject:@"latitude" forKey:[[NSString alloc] initWithFormat:@"%f", coordinates.latitude]];
+  [userInfo setObject:@"longitude" forKey:[[NSString alloc] initWithFormat:@"%f", coordinates.longitude]];
+  
+  // Setup the local notification.
+  UILocalNotification *localNotification = [[UILocalNotification alloc] init];
+  
+  // Set the notification ID and type data.
+  localNotification.userInfo = userInfo;
+  
+  // Set the notification description.
+  localNotification.alertBody = [NSString stringWithFormat:@"%@", description];
+  
+  // Set the sound alert MP3 file.
+  localNotification.soundName = [NSString stringWithFormat:@"Notification_sound_file.mp3"];
+  
+  // Set the date for the notification or set the
+  // location depending on the notification type.
+  
+  if (locationCheck == NO) {
+    
+    // Fire date of your choice.
+    NSDate *yourFireDate;
+    
+    // Set the reminder date.
+    double interval = [yourFireDate timeIntervalSinceNow];
+    localNotification.fireDate = [[NSDate date] dateByAddingTimeInterval:interval];
+    localNotification.timeZone = [NSTimeZone systemTimeZone];
+    
+    // Set the notifcation repeat interval.
+    localNotification.repeatInterval = 0; // No repeat.
+    //localNotification.repeatInterval = NSCalendarUnitHour; // Every hour.
+    //localNotification.repeatInterval = NSCalendarUnitDay; // Every day.
+    //localNotification.repeatInterval = NSCalendarUnitWeekOfYear; // Once a week.
+    //localNotification.repeatInterval = NSCalendarUnitMonth; // Once a month.
+    //localNotification.repeatInterval = NSCalendarUnitYear; // Once a year.
+  }
+  
+  else if (locationCheck == YES) {
+    
+    // Set the locaton to the selected address co-ordinates.
+    // CLLocationCoordinate2D coordinates = CLLocationCoordinate2DMake(latitude, longitude);
+    CLCircularRegion *region = [[CLCircularRegion alloc] initWithCenter:coordinates radius:100 identifier:[NSString stringWithFormat:@"region_%@", notificationID]];
+    
+    // Set the notification to be presented
+    // when the user arrives at the location.
+    [region setNotifyOnEntry:YES];
+    [region setNotifyOnExit:NO];
+    
+    // Set the notification location data.
+    [localNotification setRegion:region];
+    [localNotification setRegionTriggersOnce:NO];
+  }
+  
+  // Save the local notification.
+  [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+}
+#endif
+@end
