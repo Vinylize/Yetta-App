c370683f939004a03d2c3992e8d80606696f15ba
Yangwoo Lee <lyw0149@gmail.com>
2017-04-19 01:50:21 +0900

    Add space.

--- src/components/createOrder/bodies/findStore.js
+++ src/components/createOrder/bodies/findStore.js
@@ -90,7 +90,7 @@ export default class FindStore extends PureComponent {
     const { latitude, longitude } = this.props.coordinate;
     return client.query(`{
       viewer{
-        node (lat: ${latitude}, lon: ${longitude}, radius: 432532434234, c1:0, c2: 0) {
+        node (lat: ${latitude}, lon: ${longitude}, radius: 432532434234, c1: 0, c2: 0) {
           n,
           addr,
           distance
