7b46b56c336346b174434b1ba7d4ab6ee43b0707
Youngchan Je <ochanje210@gmail.com>
2017-04-30 17:33:42 +0900

    fixed unimported error (#109)

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import HockeyApp from 'react-native-hockeyapp';
 import store from './store';
