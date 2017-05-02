bf3d068f8915c67b36f6cd5e72d15c2527b13e7d
Yangwoo Lee <lyw0149@gmail.com>
2017-05-02 17:47:59 +0900

    Write reducer of create order.

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -5,3 +5,27 @@ export const setIsRunner = 'SET_IS_RUNNER';
 // busyWaiting
 export const setBusyWaitingPlaceDetailAPI = 'SET_BUSY_WAITING_PLACE_DETAIL_API';
 export const setBusyWaitingGeocodingAPI = 'SET_BUSY_WAITING_GEOCODING_API';
+
+// runnerStatus
+export const setWaitingNewOrder = 'SET_WAITING_NEW_ORDER';
+export const setOnDelivery = 'SET_ON_DELIEVERY';
+
+// pushNotification
+export const setRunnerNotification = 'SET_RUNNER_NOTIFICATION';
+
+// components/home
+export const setCameraWillMoveByPlaceDetailAPI = 'SET_CAMERA_WILL_MOVE_BY_PLACE_DETAIL_API';
+export const setSearchBarExpanded = 'SET_SEARCH_BAR_EXPANDED';
+export const setMapCameraPos = 'SET_MAP_CAMERA_POSITION';
+export const setShowApproveAddressCard = 'SET_SHOW_APPROVE_ADDRESS_CARD';
+export const setSearchedAddressTextView = 'SET_SEARCHED_ADDRESS_TEXT_VIEW';
+export const setCurrentLocation = 'SET_CURRENT_LOCATION';
+
+export const setStagedNode = 'SET_STAGED_NODE';
+export const addStagedItem = 'ADD_STAGED_ITEM';
+export const deleteStagedItem = 'DELETE_STAGED_ITEM';
+export const increaseStagedItemCount = 'INCREASE_STAGED_ITEM_COUNT';
+export const decreaseStagedItemCount = 'deCREASE_STAGED_ITEM_COUNT';
+export const setStagedDestination = 'SET_STAGED_DESTINATION';
+export const discardOrder = 'DISCARD_ORDER';
+
--- src/actions/createOrderActions.js
+++ src/actions/createOrderActions.js
@@ -1,8 +1,50 @@
 import * as types from './actionTypes';
 
-export const setNode = (node) => {
+export const setStagedNode = (id, name, addr) => {
   return {
-    type: types.setNode,
-    node
+    type: types.setStagedNode,
+    node: {id, name, addr}
   };
 };
+
+export const setStagedDestination = (n1, n2, lat, lon) => {
+  return {
+    type: types.setStagedDestination,
+    dest: {n1, n2, lat, lon}
+  };
+};
+
+export const addStagedItem = (item) => {
+  return {
+    type: types.addStagedItem,
+    item
+  };
+};
+
+export const deleteStagedItem = (id) => {
+  return {
+    type: types.deleteStagedItem,
+    id
+  };
+};
+
+export const increseStagedItemCount = (id) => {
+  return {
+    type: types.increaseStagedItemCount,
+    id
+  };
+};
+
+export const decreaseStagedItemCont = (id) => {
+  return {
+    type: types.decreaseStagedItemCount,
+    id
+  };
+};
+
+export const discardOrder = () => {
+  return {
+    type: types.discardOrder
+  };
+};
+
