f2e75e52ce36bc7ada85fb4e80dee055de6ac5c8
Youngchan Je <ochanje210@gmail.com>
2017-05-15 21:05:53 +0900

    fixed style on idVerification view

--- src/components/idVerification/idVerification.js
+++ src/components/idVerification/idVerification.js
@@ -11,7 +11,9 @@ import ImagePicker from 'react-native-image-picker';
 
 // assets
 import IMG_BACK from './../../../assets/left-arrow-key.png';
+import IMG_ID_CARD from './../../../assets/id-card.png';
 
+// const HEIGHT = Dimensions.get('window').height;
 const WIDTH = Dimensions.get('window').width;
 
 const styles = {
@@ -25,6 +27,9 @@ const styles = {
 class IdVerification extends Component {
   constructor() {
     super();
+    this.state = {
+      idImage: undefined
+    };
   }
 
   showImagePicker() {
@@ -43,10 +48,52 @@ class IdVerification extends Component {
         // let source = { uri: 'data:image/jpeg;base64,' + response.data };
         __DEV__ && console.log(source); // eslint-disable-line no-undef
         this.setState({
-          avatarSource: source
+          idImage: source
         });
       }
     });
+  }
+
+  renderImage() {
+    return (
+      <View>
+        <Text style={{
+          color: 'grey',
+          marginLeft: 10
+        }}>
+          * Please expose the front numbers only of your social security card.
+        </Text>
+        <View style={{
+          height: 210,
+          width: WIDTH * 0.7 + 10,
+          justifyContent: 'center',
+          alignSelf: 'center',
+          alignItems: 'center'
+        }}>
+          {(this.state.idImage) ?
+            <Image
+              style={{
+                height: 200,
+                width: WIDTH * 0.7,
+                backgroundColor: 'transparent'
+              }}
+              source={this.state.idImage}
+              resizeMode="contain"
+            />
+            :
+            <Image
+              style={{
+                height: 200,
+                width: WIDTH * 0.7,
+                backgroundColor: 'transparent'
+              }}
+              source={IMG_ID_CARD}
+              resizeMode="contain"
+            />
+          }
+        </View>
+      </View>
+    );
   }
 
   renderHeader() {
@@ -83,7 +130,7 @@ class IdVerification extends Component {
   renderBody() {
     return (
       <View style={{
-        flex: 7,
+        flex: 6,
         paddingLeft: WIDTH * 0.1,
         paddingRight: WIDTH * 0.1
       }}>
@@ -106,13 +153,66 @@ class IdVerification extends Component {
             간단히 신분증 사진만 찍으면 시작할수있어요!
           </Text>
         </View>
-        <View style={{flex: 4, backgroundColor: 'transparent'}}>
-          <TouchableOpacity
-            onPress={this.showImagePicker.bind(this)}
-          >
-            <Text>Upload</Text>
-          </TouchableOpacity>
+        <View style={{
+          flex: 4,
+          backgroundColor: 'transparent',
+          justifyContent: 'center'
+        }}>
+          {this.renderImage()}
         </View>
+      </View>
+    );
+  }
+
+  renderBottom() {
+    return (
+      <View style={{
+        flex: 2,
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        paddingLeft: WIDTH * 0.15,
+        paddingRight: WIDTH * 0.15,
+        backgroundColor: 'transparent'
+      }}>
+        <TouchableOpacity
+          style={{
+            height: 50,
+            width: WIDTH * 0.31,
+            backgroundColor: 'black',
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+          onPress={this.showImagePicker.bind(this)}
+        >
+          <Text style={{
+            backgroundColor: 'transparent',
+            color: 'white',
+            fontWeight: '500'
+          }}>
+            {(this.state.idImage) ? '다시올리기' : '업로드하기'}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            height: 50,
+            width: WIDTH * 0.31,
+            backgroundColor: (this.state.idImage) ? 'black' : 'grey',
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+          activeOpacity={(this.state.idImage) ? 0.7 : 1}
+        >
+          <Text style={{
+            backgroundColor: 'transparent',
+            color: 'white',
+            fontWeight: '500'
+          }}>
+            제출하기
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -122,6 +222,7 @@ class IdVerification extends Component {
       <View style={styles.container}>
         {this.renderHeader()}
         {this.renderBody()}
+        {this.renderBottom()}
       </View>
     );
   }
