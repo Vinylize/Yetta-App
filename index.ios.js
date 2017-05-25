94a09b4c537cba5312010aa580a4dfcb4de646cc
Youngchan Je <ochanje210@gmail.com>
2017-05-26 01:42:40 +0900

    [Android] get android google maps api key from .env file

--- index.ios.js
+++ index.ios.js
@@ -4,7 +4,13 @@ import * as firebase from 'firebase';
 import Config from 'react-native-config';
 
 __DEV__ && console.log(Config.firebaseConfig); // eslint-disable-line no-undef
-const jsonFirebaseConfig = JSON.parse(Config.firebaseConfig);
+const jsonFirebaseConfig = {
+  apiKey: Config.firebaseConfig_apiKey,
+  authDomain: Config.firebaseConfig_authDomain,
+  databaseURL: Config.firebaseConfig_databaseURL,
+  storageBucket: Config.firebaseConfig_storageBucket,
+  messagingSenderId: Config.firebaseConfig_messagingSenderId
+};
 firebase.initializeApp(jsonFirebaseConfig);
 
 AppRegistry.registerComponent('pingstersApp', () => Vinyl);
